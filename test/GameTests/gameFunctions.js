b3706c719d438d4dac1dc788d587c555b49377e4
Julian Brown <julian@the-lair.org.uk>
2016-08-28 09:36:18 +0100

    refactored spec tests to reduce lines of code and duplication

--- test/GameTests/gameFunctions.js
+++ test/GameTests/gameFunctions.js
@@ -33,16 +33,13 @@ describe("Game Functions", function() {
 		var callCounter;
 		var results = [];
 
-		function notRnd(x) {
+		function notRnd() {
 			if (callCounter < results.length) return results[callCounter++]; else return 1;
 		}
 
-		// needs attributes, inventory, inventory counter, monster index, monster stats, terminal (mock), stateMachine (mock)
 		beforeEach(function() {
 			callCounter = 0;
-			//"HP", "STR", "DEX", "CON", "CHAR", "WIS", "INT", "GOLD"
 			attributes = [10, 10, 10, 10, 10, 10, 10, 1000];
-			//need a value of 8, 9 or 10 in the inventory array for testing
 			inventory = [];
 			inventoryCounter = 0;
 			currentMonster = 0;
@@ -72,127 +69,108 @@ describe("Game Functions", function() {
 		});
 
 		describe('Calculates chance to hit against protection', function() {
-			it("calculates a hit against no armour", function() {
+			beforeEach(function() {
 				currentMonster = 1;
-				results = [17,1];
 				spyOn(window, 'rnd').and.callFake(notRnd);
+			});
+
+			it("calculates a hit against no armour", function() {
+				results = [17,1];
 				monsterSwings();
 				expect(gameStateMachine.stateMode === 200).toBe(true);
 				expect(terminal.lastInput === ("H.P.=" + attributes[0])).toBe(true);
 			});
 
 			it("calculates a minor miss against no armour", function() {
-				currentMonster = 1;
 				results = [15,2];
-				spyOn(window, 'rnd').and.callFake(notRnd);
 				monsterSwings();
 				expect(gameStateMachine.stateMode === 200).toBe(true);
 				expect(terminal.lastInput === "HE HIT BUT NOT GOOD ENOUGH").toBe(true);
 			});
 
 			it("calculates a critical miss against no armour", function() {
-				currentMonster = 1;
 				results = [15,0];
-				spyOn(window, 'rnd').and.callFake(notRnd);
 				monsterSwings();
 				expect(gameStateMachine.stateMode === 25).toBe(true);
 				expect(terminal.lastInput === "HE MISSED").toBe(true);
 			});
 
 			it("calculates a hit against leather armour", function() {
-				currentMonster = 1;
 				inventory = [0,8];
 				inventoryCounter = 1;
 				results = [19,1];
-				spyOn(window, 'rnd').and.callFake(notRnd);
 				monsterSwings();
 				expect(gameStateMachine.stateMode === 200).toBe(true);
 				expect(terminal.lastInput === ("H.P.=" + attributes[0])).toBe(true);
 			});
 
 			it("calculates a minor miss against leather armour", function() {
-				currentMonster = 1;
 				inventory = [0,8];
 				inventoryCounter = 1;
 				results = [15,2];
-				spyOn(window, 'rnd').and.callFake(notRnd);
 				monsterSwings();
 				expect(gameStateMachine.stateMode === 200).toBe(true);
 				expect(terminal.lastInput === "HE HIT BUT NOT GOOD ENOUGH").toBe(true);
 			});
 
 			it("calculates a critical miss against leather armour", function() {
-				currentMonster = 1;
 				inventory = [0,8];
 				inventoryCounter = 1;
 				results = [15,0];
-				spyOn(window, 'rnd').and.callFake(notRnd);
 				monsterSwings();
 				expect(gameStateMachine.stateMode === 25).toBe(true);
 				expect(terminal.lastInput === "HE MISSED").toBe(true);
 			});
 
 			it("calculates a hit against chain armour", function() {
-				currentMonster = 1;
 				inventory = [0,9];
 				inventoryCounter = 1;
 				results = [27,1];
-				spyOn(window, 'rnd').and.callFake(notRnd);
 				monsterSwings();
 				expect(gameStateMachine.stateMode === 200).toBe(true);
 				expect(terminal.lastInput === ("H.P.=" + attributes[0])).toBe(true);
 			});
 
 			it("calculates a minor miss against chain armour", function() {
-				currentMonster = 1;
 				inventory = [0,9];
 				inventoryCounter = 1;
 				results = [25,2];
-				spyOn(window, 'rnd').and.callFake(notRnd);
 				monsterSwings();
 				expect(gameStateMachine.stateMode === 200).toBe(true);
 				expect(terminal.lastInput === "HE HIT BUT NOT GOOD ENOUGH").toBe(true);
 			});
 
 			it("calculates a critical miss against chain armour", function() {
-				currentMonster = 1;
 				inventory = [0,9];
 				inventoryCounter = 1;
 				results = [15,0];
-				spyOn(window, 'rnd').and.callFake(notRnd);
 				monsterSwings();
 				expect(gameStateMachine.stateMode === 25).toBe(true);
 				expect(terminal.lastInput === "HE MISSED").toBe(true);
 			});
 
 			it("calculates a hit against plate armour", function() {
-				currentMonster = 1;
 				inventory = [0,10];
 				inventoryCounter = 1;
 				results = [31,1];
-				spyOn(window, 'rnd').and.callFake(notRnd);
 				monsterSwings();
 				expect(gameStateMachine.stateMode === 200).toBe(true);
 				expect(terminal.lastInput === ("H.P.=" + attributes[0])).toBe(true);
 			});
 
 			it("calculates a minor miss against plate armour", function() {
-				currentMonster = 1;
 				inventory = [0,10];
 				inventoryCounter = 1;
 				results = [30,2];
-				spyOn(window, 'rnd').and.callFake(notRnd);
 				monsterSwings();
 				expect(gameStateMachine.stateMode === 200).toBe(true);
 				expect(terminal.lastInput === "HE HIT BUT NOT GOOD ENOUGH").toBe(true);
 			});
 
 			it("calculates a critical miss against plate armour", function() {
-				currentMonster = 1;
 				inventory = [0,10];
 				inventoryCounter = 1;
 				results = [30,0];
-				spyOn(window, 'rnd').and.callFake(notRnd);
 				monsterSwings();
 				expect(gameStateMachine.stateMode === 25).toBe(true);
 				expect(terminal.lastInput === "HE MISSED").toBe(true);
